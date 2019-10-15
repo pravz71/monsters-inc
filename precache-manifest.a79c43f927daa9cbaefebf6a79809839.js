4421fc53543023c190760e7c586dae4e465647ca
Pravin Gupta <pravingupta1071@gmail.com>
2019-10-15 18:34:06 +0530

    Updates

---
+++ precache-manifest.a79c43f927daa9cbaefebf6a79809839.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f14cf6857fede190ae048c2122ce95da",
+    "url": "/monsters-inc/index.html"
+  },
+  {
+    "revision": "ecf34a4eec9e86b5e9c0",
+    "url": "/monsters-inc/static/css/main.3f46f041.chunk.css"
+  },
+  {
+    "revision": "17b0b51a3e9f899a6450",
+    "url": "/monsters-inc/static/js/2.c02f2227.chunk.js"
+  },
+  {
+    "revision": "ecf34a4eec9e86b5e9c0",
+    "url": "/monsters-inc/static/js/main.dd174b13.chunk.js"
+  },
+  {
+    "revision": "1b2c648cbcc46a3d5bfc",
+    "url": "/monsters-inc/static/js/runtime-main.c60d8569.js"
+  }
+]);
